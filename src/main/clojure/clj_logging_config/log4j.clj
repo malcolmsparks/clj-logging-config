750419e3bf43581225b0742f479800e0ca1d8416
Malcolm Sparks <malcolm@congreve.com>
2011-07-18 13:08:42 +0100

    Numerous enhancements.
    1. (as-map) used for filter function.
    2. Renamed :appender to :out wherever it made sense, to reduce difference with
       java.util.logging, and allowed :out to be a Clojure writer.
    3. Replaced PatternLayout with EnhancedPatternLayout, as recommeded by the Log4j javadoc.
    4. Added support for :header and :footer (although I have not seen this working yet
       and it needs to be specifically tested for).
    5. It is now possible to pass a logger implementation, not just the logger name -
       this is useful for passing the root logger.
    6. Code simplifications.

--- src/main/clojure/clj_logging_config/log4j.clj
+++ src/main/clojure/clj_logging_config/log4j.clj
@@ -11,25 +11,15 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns clj-logging-config.log4j
-  (:import (org.apache.log4j Logger ConsoleAppender PatternLayout Level
+  (:import (org.apache.log4j Logger ConsoleAppender EnhancedPatternLayout Level
                              LogManager AppenderSkeleton Appender Layout
-                             SimpleLayout)))
+                             SimpleLayout WriterAppender)))
 
 (defn reset-logging []
   (LogManager/resetConfiguration))
 
 (defn as-map [^LoggingEvent ev]
   (assoc (bean ev) :event ev))
-
-(defn wrap-appender-with-filter
-  ([^Appender delegate filterfn ^String name]
-     (proxy [AppenderSkeleton] []
-       (append [^LoggingEvent ev]
-               (when (filterfn (as-map ev)) (.append delegate ev)))
-       (getName [] name)
-       (close [] (.close delegate))))
-  ([^Appender delegate filterfn]
-     (wrap-appender-with-filter delegate filterfn nil)))
 
 (defn create-formatter-from-layout
   ([^Layout layout ^String name]
@@ -40,6 +30,14 @@
   ([^Layout layout]
      (create-formatter-from-layout layout nil)))
 
+(defn create-appender
+  ([f ^String name]
+     (proxy [AppenderSkeleton] []
+       (append [^LoggingEvent ev] (f (as-map ev)))
+       (getName [] name)
+       (close [] nil)))
+  ([f] (create-appender f nil)))
+
 (defn create-repl-appender
   ([^Layout layout ^String name]
      (proxy [AppenderSkeleton] []
@@ -49,17 +47,26 @@
   ([^Layout layout]
      (create-repl-appender layout nil)))
 
-(defn create-appender
-  ([f ^String name]
+(defn wrap-appender-with-filter
+  ([^Appender delegate filterfn]
      (proxy [AppenderSkeleton] []
-       (append [^LoggingEvent ev] (f (as-map ev)))
-       (getName [] name)
-       (close [] nil)))
-  ([f] (create-appender f nil)))
+       (append [^LoggingEvent ev]
+               (when (filterfn (as-map ev)) (.append delegate ev)))
+       (getName [] (.getName delegate))
+       (close [] (.close delegate)))))
+
+(defn wrap-appender-with-header-and-footer
+  ([^Appender delegate header footer]
+     (proxy [AppenderSkeleton] []
+       (append [^LoggingEvent ev] (.append delegate ev))
+       (getName [] (.getName delegate))
+       (getHeader [] (if (fn? header) (header) (str header)))
+       (getFooter [] (if (fn? footer) (footer) (str footer)))
+       (close [] (.close delegate)))))
 
 (defn create-layout [formatter]
   (proxy [Layout] []
-    (format [ev] (formatter ev))))
+    (format [ev] (formatter (as-map ev)))))
 
 (defn as-level [level]
   (cond
@@ -73,35 +80,50 @@
                           :all Level/ALL} level)
    (instance? Level level) level))
 
+(defn wrap-for-filter [filterfn]
+  (fn [appender]
+    (if filterfn
+      (wrap-appender-with-filter appender filterfn)
+      appender)))
+
+(defn wrap-for-header-or-footer [header footer]
+  (fn [appender]
+    (if (or header footer)
+      (wrap-appender-with-header-and-footer appender header footer)
+      appender)))
+
 (defn ^{:private true}
   set-logger
-  [[logger {:keys [appender-name level appender pattern layout filter additivity]
-            :or {appender-name "_default" additivity true}}]]
-  (cond (and appender (or layout pattern))
-        (throw (IllegalStateException. "Cannot specify an :appender and one of :pattern and :layout"))
-        (and layout pattern)
-        (throw (IllegalStateException. "Cannot specify both :pattern and :layout")))
+  [[logger {:keys [name level writer pattern layout filter additivity header footer]
+            :or {name "_default" additivity true}}]]
+  (when (and layout pattern)
+    (throw (IllegalStateException. "Cannot specify both :pattern and :layout")))
 
-  (let [logger (Logger/getLogger ^String logger)
+  (let [logger (if (string? logger) (Logger/getLogger ^String logger) logger) 
 
-        ^Layout actual-layout (cond
-                               (fn? layout) (create-layout layout)
-                               pattern (PatternLayout. pattern)
-                               layout layout)
-        ^Appender appender-delegate (cond
-                                     (fn? appender) (create-appender appender appender-name)
-                                     appender appender
-                                     actual-layout (create-repl-appender actual-layout appender-name)
-                                     :otherwise (create-repl-appender (SimpleLayout.) appender-name))
-        ^Appender final-appender (cond
-                                  filter (wrap-appender-with-filter appender-delegate filter appender-name)
-                                  :otherwise appender-delegate)]
-    (if (nil? (.getName final-appender))
-      (.setName final-appender appender-name))
+        ^Layout actual-layout
+        (cond
+         (fn? layout) (create-layout layout)
+         pattern (EnhancedPatternLayout. pattern)
+         layout layout
+         :otherwise nil)
+
+        ^Appender appender
+        (cond
+         (instance? Appender writer) writer
+         (fn? writer) (create-appender writer name)
+         (instance? java.io.Writer writer) (WriterAppender. actual-layout writer)
+         writer (throw (IllegalStateException. (format "Wrong type of appender: %s" (type writer))))
+         actual-layout (create-repl-appender actual-layout name)
+         :otherwise (create-repl-appender (SimpleLayout.) name))]
+
+    (if (nil? (.getName appender))
+      (.setName appender name))
+
     (doto logger
-      (.removeAppender appender-name)
+      (.removeAppender name)
       (.setAdditivity additivity)
-      (.addAppender final-appender)
+      (.addAppender ((comp (wrap-for-filter filter) (wrap-for-header-or-footer header footer)) appender))
       (.setLevel (or (as-level level) Level/INFO)))))
 
 ;; To grok this, see http://briancarper.net/blog/579/keyword-arguments-ruby-clojure-common-lisp
